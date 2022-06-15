a2f366b19f7f396d6f053c634867bb70a75fe3c7
Aviatorscode2 <victoryakubu5@gmail.com>
2022-06-15 03:35:01 +0100

    implemented the Offers page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,8 +1,82 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
 
 function Offers() {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const q = query(
+          collection(db, 'listings'),
+          where('offer', '==', true)
+        );
+
+        const querySnapshot = await getDocs(q);
+
+        const listings = [];
+
+        querySnapshot.forEach((doc) => {
+          // console.log(doc.data())
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Could not fetch listings');
+      }
+    };
+
+    fetchListings();
+  }, []);
+
   return (
-    <h1>Offers</h1>
-  )
+    <div className="category">
+      <header>
+        <p className="pageHeader">
+          Offers
+        </p>
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There are no current offers, try later</p>
+      )}
+    </div>
+  );
 }
 
-export default Offers
+export default Offers;
