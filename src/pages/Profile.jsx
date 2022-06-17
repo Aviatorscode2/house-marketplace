021b3436b80b9d7d5aa8e4c490a45a93f4cde843
Aviatorscode2 <victoryakubu5@gmail.com>
2022-06-17 16:01:04 +0100

    Functionality to delete listing

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -80,8 +80,14 @@ function Profile() {
     }))
   }
 
-  const onDelete = () => {
-    
+  const onDelete = async (listingId) => {
+    if(window.confirm('Are you sure you want to delete this listing?')) {
+      await deleteDoc(doc(db, 'listings', listingId))
+      const updatedListings = listings.filter((listing) => listing.id !== listingId)
+
+      setListings(updatedListings)
+      toast.success('Successfully deleted listing')
+    }
   }
   return (
     <div className="profile">
