0f9f42f284409b36463c75076c48224c0a575f38
Aviatorscode2 <victoryakubu5@gmail.com>
2022-06-17 17:44:37 +0100

    Added pagination feature on the category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -18,17 +18,20 @@ import ListingItem from '../components/ListingItem';
 function Category() {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [lastFetchedListing, setLastFetchedListing] = useState(null)
 
   const params = useParams()
 
   useEffect(() => {
     const fetchListings = async () => {
       try {
-
-        const q = query(collection(db, 'listings'), where('type', '==', params.categoryName))
+        const listingRef = collection(db, 'listings')
+        const q = query(listingRef, where('type', '==', params.categoryName), limit(1))
 
         const querySnapshot = await getDocs(q)
 
+        const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1]
+        setLastFetchedListing(lastVisible)
         const listings = []
 
         querySnapshot.forEach((doc) => {
@@ -48,6 +51,44 @@ function Category() {
     fetchListings()
   }, [params.categoryName])
 
+  // pagination / Load more
+
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, 'listings');
+
+      // Create a query
+      const q = query(
+        listingsRef,
+        where('type', '==', params.categoryName),
+        startAfter(lastFetchedListing),
+        limit(1)
+      );
+
+      // Execute query
+      const querySnap = await getDocs(q);
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+
+      const listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error('Could not fetch listings');
+    }
+  };
+  
+
   return (
     <div className="category">
       <header>
@@ -64,6 +105,11 @@ function Category() {
             ))}
           </ul>
         </main>
+        <br />
+        <br />
+        {lastFetchedListing && (
+          <p className="loadMore" onClick={onFetchMoreListings}>Load More</p>
+        )}
       </> : <p>No listings {params.categoryName}</p>}
     </div>
   )
