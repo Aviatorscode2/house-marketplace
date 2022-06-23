bdcceddf1c7ba2a75e530a01c4c258fbda49dea7
Aviatorscode2 <victoryakubu5@gmail.com>
2022-06-23 17:21:53 +0100

    Installed Swiper and Added it to the Listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,12 +1,18 @@
 import {useState, useEffect} from 'react'
 import {Link, useNavigate, useParams} from 'react-router-dom'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css'
+// import 'swiper/css/bundle'
 import {getDoc, doc} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 import {db} from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
 import {toast } from 'react-toastify'
+ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
 
 function Listing() {
     const  [listing, setListing] = useState(null)
@@ -38,6 +44,19 @@ function Listing() {
 
   return (
     <main>
+      {listing.imgUrls ? <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper> : 'No Imgages to display'}
       <div
         className="shareIconDiv"
         onClick={() => {
