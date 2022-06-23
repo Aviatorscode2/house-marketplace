288269518dd1b77603a9b652b911f8d09f72bc7b
Aviatorscode2 <victoryakubu5@gmail.com>
2022-06-23 17:12:36 +0100

    Fixing Swiper.Js bug by removing React.strict mode

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // Deleted React.Strict mode to allow swiper.js work properly, it was affecting it
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
